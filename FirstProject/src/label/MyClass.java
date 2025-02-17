f06713863993b9a183c7e1df53396bdad6ed102d
Sushmashreeps10 <sushre17@gmail.com>
2025-02-17 23:47:30 +0530

    basic structure of java file

---
+++ FirstProject/src/label/MyClass.java
@@ -0,0 +1,37 @@
+package label;
+
+import java.util.Scanner;
+
+public class MyClass {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter some number");
+		String Str1 = sc.nextLine();
+		
+		System.out.println("enter new number");
+		System.out.print(Str1);
+		
+		
+		
+		int my_val = 10;
+		short vbl = 184;
+		float vbl1 = (float)169.1;
+		double vb3 = 184.11;
+		char vbl22 = 'a';
+		boolean trueorfalse = true;
+		
+		System.out.println(my_val);
+		System.out.print(vbl);
+		System.out.println(vbl1);
+		System.out.println(vb3);
+		System.out.println(vbl22);		
+		System.out.println(trueorfalse);
+		
+		
+		
+		
+	}
+	
+}
