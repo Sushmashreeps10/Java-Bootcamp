19e5ae9241f9083b0c41761215854e15cbfcb7ac
Sushmashreeps10 <sushre17@gmail.com>
2025-02-18 12:42:08 +0530

    mathematical operations in Java
    Addition, sub, multiplication, division
    increment operator

---
+++ MathOperators/src/clasi/Caluculator.java
@@ -0,0 +1,36 @@
+package clasi;
+
+public class Caluculator {
+
+	public static void main(String[] args) {
+		// Math operator
+		int x, y, ans;
+		x = 1;
+		y = 2;
+		ans = x * y;
+		int aans = x + y;
+		double ans1 = x % y;
+		float ans2 = x / y;
+		
+		System.out.println("Answer is = " + ans);
+		System.out.println("Answer is = " + aans);
+		System.out.println("Answer is = " + ans1);
+		System.out.println("Answer is = " + ans2);
+		
+//		incrementation
+		
+		int x1 = 10;
+		x1 *= 5;
+		
+//		x++;
+		System.out.println(x1++);
+		System.out.println(x1);
+		
+		System.out.println(x1);
+		
+		
+		
+
+	}
+
+}
