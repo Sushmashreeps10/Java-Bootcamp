333c31c493d01da5fc458b6de0cbe8d91c86fd89
Sushmashreeps10 <sushre17@gmail.com>
2025-02-18 23:07:20 +0530

    Factorial of a number

---
+++ RecursionFactorialOfANum/src/factorialOfANumber/Factorial.java
@@ -0,0 +1,17 @@
+package factorialOfANumber;
+
+public class Factorial {
+	
+	public static int fact(int a) {
+		if (a <= 1) {
+			return 1;
+		}else {
+			return(a * fact(a-1));
+		}
+	}
+	public static void main(String[] args) {
+		System.out.println(fact(5));
+		
+	}
+
+}
