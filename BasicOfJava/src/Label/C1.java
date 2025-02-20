df1f5dcad072bb1ace9e3c9b1ba56989eb997182
Sushmashreeps10 <sushre17@gmail.com>
2025-02-20 22:04:37 +0530

    java basics are inclued

---
+++ BasicOfJava/src/Label/C1.java
@@ -0,0 +1,27 @@
+package Label;
+
+public class C1 {
+	
+	public static void main (String[] args) {
+		int x = 9/2;
+		
+		System.out.println("the value is :" +" "+x);
+		System.out.println(9/2f);//float
+		System.out.println(10/3d);//double
+		
+		boolean var = true;
+		System.out.println(var);
+		
+		char var1 = 'a';
+		char var2 = 'f';
+		String v1 = "100";
+		String v2 = "893"; //string is a collection of char
+		
+		System.out.println("char values are :" +  var1+var2);
+		System.out.println(var1+var2);
+		System.out.println("value of our string : " + " "+ v1+v2);
+	}
+	
+	
+
+}
