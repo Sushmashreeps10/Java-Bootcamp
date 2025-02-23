956bd8b2e5ddccfa2b6f46c6a82fbcd2e59b9090
Sushmashreeps10 <sushre17@gmail.com>
2025-02-23 15:40:07 +0530

    Methods in java

---
+++ PraticeProject/src/Package/MyClass.java
@@ -0,0 +1,38 @@
+package Package;
+
+public class MyClass {
+	
+	public static void main(String[] args) {
+//	     mymethod(10,20);
+//	     System.out.println("--------------");
+//	     mymethod(300,200);
+//	     
+//		
+//	}
+//	
+//	public static void mymethod(int start,int stop) {
+//		for(;start>stop;start++){
+//			System.out.println(start);
+//			break;
+//		}
+//		
+		
+		int n = 345678;
+		
+		switch(n) {
+		case 1:
+			System.out.println("will you print case1");
+			break;
+		case 2:
+			System.out.println("will you print case2");
+			break;
+		case 3:
+			System.out.println("Will you print case3");
+			break;
+		default:
+			System.out.println("offcause I'm your last option");
+		}
+		
+	}
+
+}
